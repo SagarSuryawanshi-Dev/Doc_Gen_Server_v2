e1c9d0daeb97f0f797a0efcf4b3c398f539ed77f
Sagar <sagarsurya45@gmail.com>
2026-04-09 16:55:17 +0530

    employee  Schema

--- src/config/db.js
+++ src/config/db.js
@@ -9,16 +9,16 @@ const dbConnection = async () => {
 
     try {
       await collection.dropIndex("company_1_employeeName_1_joiningDate_1");
-      console.log("✅ Dropped employeeName index");
+      console.log(" Dropped employeeName index");
     } catch (err) {
-      console.log("⚠️ employeeName index not found");
+      console.log(" employeeName index not found");
     }
 
     try {
       await collection.dropIndex("company_1_candidateName_1_joiningDate_1");
-      console.log("✅ Dropped candidateName index");
+      console.log(" Dropped candidateName index");
     } catch (err) {
-      console.log("⚠️ candidateName index not found");
+      console.log(" candidateName index not found");
     }
   } catch (error) {
     console.log("Database Connection Failed", error.message);
