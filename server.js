ddd505061244c136a6e1e0085fa9b0bc415b243a
Sagar <sagarsurya45@gmail.com>
2026-04-16 14:27:21 +0530

    update server while listening

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ import app from "./app.js";
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server Versin 2 is running on port ${PORT}`);
+  console.log(`Server Version 2 is running on port ${PORT}`);
 });
